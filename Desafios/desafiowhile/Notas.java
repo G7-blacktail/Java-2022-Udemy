876360dee4b1e87ec26bcf02353ecd31c7a98cb1
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-01-25 17:52:38 -0300

    "aula 74"

---
+++ Desafios/desafiowhile/Notas.java
@@ -0,0 +1,20 @@
+package desafiowhile;
+
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double total;
+
+        int exit = 0;
+
+         while(exit != -1){
+            double nota;
+            
+         }
+
+        entrada.close();
+    }
+}
+
