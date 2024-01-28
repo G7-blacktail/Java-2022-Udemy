b05352c47f1436401af9e1b515daef5099ddf1d8
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-01-28 18:46:03 -0300

    "Finalizado resolução do desafio while"

--- estudos/desafios/desafiowhile/Notas.java
+++ estudos/desafios/desafiowhile/Notas.java
@@ -5,14 +5,30 @@ import java.util.Scanner;
 public class Notas {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double total;
-
+        double total = 0d;
+        int numeroDeAlunos = 0;
         int exit = 0;
 
          while(exit != -1){
-            double nota;
-            
+            double nota = 0;
+            System.out.println("Insira a nota do aluno: ");
+               nota = entrada.nextDouble();
+            if( nota >= 0 && nota <= 10){
+                numeroDeAlunos++;
+                total += nota;
+            } else if (nota == -1){
+                System.out.println("Finalizado coleta de notas");
+                exit = (int) nota;
+            }
+
          }
+
+         System.out.println("Numero de alunos da classe: " + numeroDeAlunos);
+
+         double media = total / numeroDeAlunos;
+
+         System.out.println("Media total da classe: " + media);
+
 
         entrada.close();
     }
