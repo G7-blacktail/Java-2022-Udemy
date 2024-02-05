969792301b898fe0bd8919a8991a4a02537f0548
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-02-05 00:01:24 -0300

    "Finalizando estruturas de controle"

---
+++ estudos/estruturas_de_repeticao/break_continue/BreakLeanning.java
@@ -0,0 +1,15 @@
+package estruturas_de_repeticao.break_continue;
+
+public class BreakLeanning {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++){
+            if(i == 5){
+                break;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("FiM");
+    }
+}
---
+++ estudos/estruturas_de_repeticao/break_continue/BreakRotulado.java
@@ -0,0 +1,17 @@
+package estruturas_de_repeticao.break_continue;
+
+public class BreakRotulado {
+    public static void main(String[] args) {
+        externo: for(int i = 0; i < 3; i++){ //rotulando um laço para o break agir nele
+            for(int j = 0; j < 3; j++){
+
+                if(i == 1) break externo;
+
+                System.out.printf("[%d , %d] ", i, j);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Fim");
+    }
+}
